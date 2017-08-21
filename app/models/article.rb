a46ffe00dd81c7580bbca5d9b954a74856e891eb
macro6461 <mattcroak718@gmail.com>
2017-08-21 10:36:10 -0400

    Done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,26 @@
 class Article
+  attr_accessor :title, :description, :url, :contributor, :time_published
 
+  @@all = []
+
+  def initialize(title, description, url, contributor, time_published)
+    @title = title
+    @description = description
+    @url = url
+    @contributor = contributor
+    @time_published = time_published
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_all_by_category(category) #category is in adapter
+    Adapter.all.select do |adapter|
+       adapter.article = self
+     end
+  end
 
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,36 @@
 class Category
 
+  attr_accessor :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create(category_name)
+    self.find(category_name) ? self.find(category_name) : self.create(category_name)
+  end
+
+  def self.top_category
+    
+  end
+
+  ########### HELPER METHODS ##############
+
+ def self.find(category_name)
+   self.all.find do |category|
+     category.name == category_name
+   end
+ end
+
+ def self.create(category_name)
+   Category.new(category_name)
+ end
 
 end
